2376f977755b85913a50270b05858db85ab6d314
ashutosh-2002 <83767726+ashutosh-2002@users.noreply.github.com>
2023-05-08 17:13:36 +0530

    Update question4.js

--- question4.js
+++ question4.js
@@ -26,7 +26,7 @@ function submit() {
     if (taskInput.value == "720") {
         let score = parseInt(localStorage.getItem('score'));
         let currtime = parseInt(localStorage.getItem('time'));
-        score += 10;
+        score += 20;
         score += Math.floor(currtime / 60);
         score += (currtime % 60) / 100;
 
