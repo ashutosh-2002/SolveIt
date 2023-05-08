bcb546369a53ec26a39664e6ab0e93178b6e9c29
ashutosh-2002 <83767726+ashutosh-2002@users.noreply.github.com>
2023-05-08 16:02:58 +0530

    Delete question3.js

--- question3.js
+++
@@ -1,9 +0,0 @@
-const taskInput = document.querySelector("#task");
-
-function addState() {
-
-    if (taskInput.value == "a1")
-        location.assign("question2.html");
-    // else
-    //     location.assign("a.html");
-}
