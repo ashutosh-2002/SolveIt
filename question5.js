d685a34fd76666cd17b01f0b1d6c0b11c6d8d454
ashutosh-2002 <83767726+ashutosh-2002@users.noreply.github.com>
2023-05-08 17:44:56 +0530

    Update question5.js

--- question5.js
+++ question5.js
@@ -35,6 +35,9 @@ function submit() {
         location.assign("result.html");
     }
     else {
+        let score = parseInt(localStorage.getItem('score'));
+        score -= 5;
+        localStorage.setItem('score', score);
         error.style.display = 'block';
     }
 }
