b8959f74ef48357d8cd1d6a7cec210e7e69352dc
ashutosh-2002 <83767726+ashutosh-2002@users.noreply.github.com>
2023-05-08 17:58:49 +0530

    Update AdminSignIn.js

--- AdminSignIn.js
+++ AdminSignIn.js
@@ -3,12 +3,28 @@ const name = document.querySelector('#name');
 const password = document.querySelector('#password');
 const message = document.querySelector('#message');
 const hidden = document.querySelector('#hidden');
+const error = document.querySelector('#error');
 
+function showError(msg) {
+
+    error.textContent = msg;
+    error.style.display = 'block';
+
+}
 
 function SignIn() {
 
-    if (email.value == 'admin@gmail.com' && password.value == '12345') {
+    if (email.value == "")
+        showError("Please enter Email Id !!!");
+
+    else if (password.value == "")
+        showError("Please enter Password !!!");
+
+    else if (email.value == 'admin@gmail.com' && password.value == '12345') {
         location.assign('admin.html');
     }
+
+    else
+        showError("Incorrect Email Id or Password !!!");
 
 }
